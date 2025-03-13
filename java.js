cb2bc8084ac34d304b25a6597896abf469ebe452
Rustam-7 <joraqulovamahfuza7@gmail.com>
2025-03-13 22:08:32 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,37 @@
+let kaka = prompt('String typedagi malumot kiriting' );
+console.log ('siz' + typeof nimadir + 'stringdagi malumot kiritingiz')
+
+
+
+
+
+let num = prompt('number typedagi malumot kiriting' );
+console.log ('siz' + typeof num + 'numberdagi malumot kiritingiz')
+
+
+
+
+
+let bol = prompt('boolean typedagi malumot kiriting' );
+console.log ('siz' + typeof bol + 'booleandagi malumot kiritingiz')
+
+
+if (confirm("Haqiqatan ham ushbu harakatni bajarishni xohlaysizmi?")) {
+    console.log("Foydalanuvchi OK tugmasini bosdi.");
+} else {
+    console.log("Foydalanuvchi Bekor qilish tugmasini bosdi.");
+}
+
+
+let ohirgisi = alert('siz kiritgan "Farux" sozi string typega tegishli                                      siz kiritgan "123 "number typega tegishli                                              siz kiritgan "false yoke True " boolean typega tegishli'  
+   
+)
+console.log('ohirgisi');
+
+
+
+
+
+
+
+
